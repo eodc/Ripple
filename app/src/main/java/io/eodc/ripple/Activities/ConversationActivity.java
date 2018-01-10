2b46360445c5b5a7353da71a79e279ff6095e81a
eodc <nicholasnhien@gmail.com>
2018-01-09 21:36:11 -0800

    Make message composer a drawer for attachments

--- app/src/main/java/io/eodc/ripple/Activities/ConversationActivity.java
+++ app/src/main/java/io/eodc/ripple/Activities/ConversationActivity.java
@@ -1,13 +1,19 @@
 package io.eodc.ripple.Activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.r0adkll.slidr.Slidr;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import io.eodc.ripple.BuildConfig;
 import io.eodc.ripple.R;
@@ -16,7 +22,11 @@ import timber.log.Timber;
 public class ConversationActivity extends AppCompatActivity {
 
     SmsManager smsManager;
-    EditText mMsgComposer;
+
+    SlidingUpPanelLayout slidingLayout;
+    ImageView mAttachIcon;
+    EditText mMessageComposer;
+    FloatingActionButton mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +46,69 @@ public class ConversationActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Timber.i("Toolbar initialized");
 
-        mMsgComposer =  findViewById(R.id.message_composer);
+        slidingLayout = findViewById(R.id.root_layout);
+        mAttachIcon = findViewById(R.id.attach_icon);
+        mAttachIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (slidingLayout.getPanelState() == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+                    slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+                } else if (slidingLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED) {
+                    slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+                }
+            }
+        });
+        mMessageComposer = findViewById(R.id.message_composer);
+        mMessageComposer.addTextChangedListener(new TextLineCountListener());
+
+        mSendButton = findViewById(R.id.sendButton);
     }
 
     public void sendMessage(View view) {
+    }
+
+    class TextLineCountListener implements TextWatcher {
+        int numLines = 1;
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            if (numLines < mMessageComposer.getLineCount() && mMessageComposer.getLineCount() <= 5) {
+                numLines = mMessageComposer.getLineCount();
+                int deltaHeight = mMessageComposer.getHeight() / numLines;
+                slidingLayout.setPanelHeight((slidingLayout.getPanelHeight() + deltaHeight));
+
+                RelativeLayout.LayoutParams attachIconParams = (RelativeLayout.LayoutParams) mAttachIcon.getLayoutParams();
+                attachIconParams.topMargin = attachIconParams.topMargin + deltaHeight / 2;
+                mAttachIcon.setLayoutParams(attachIconParams);
+
+                RelativeLayout.LayoutParams sendButtonParams = (RelativeLayout.LayoutParams) mSendButton.getLayoutParams();
+                sendButtonParams.topMargin = sendButtonParams.topMargin + deltaHeight / 2;
+                mSendButton.setLayoutParams(sendButtonParams);
+
+            } else if (numLines > mMessageComposer.getLineCount()) {
+                numLines = mMessageComposer.getLineCount();
+                int deltaHeight = slidingLayout.getPanelHeight() - mMessageComposer.getHeight();
+                slidingLayout.setPanelHeight((mMessageComposer.getHeight()));
+
+                RelativeLayout.LayoutParams attachIconParams = (RelativeLayout.LayoutParams) mAttachIcon.getLayoutParams();
+                attachIconParams.topMargin = attachIconParams.topMargin - deltaHeight / 2;
+                mAttachIcon.setLayoutParams(attachIconParams);
+
+                RelativeLayout.LayoutParams sendButtonParams = (RelativeLayout.LayoutParams) mSendButton.getLayoutParams();
+                sendButtonParams.topMargin = sendButtonParams.topMargin - deltaHeight / 2;
+                mSendButton.setLayoutParams(sendButtonParams);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
     }
 }
 
